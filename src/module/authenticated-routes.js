14ddc0413aeff0bae3ed6fb9c2199cf11b8729dc
Tognozzi <gabrio.tognozzi@moveax.it>
2021-03-14 22:23:48 +0100

    setup authenticated routes and implement passport linkedin strategy

--- src/module/authenticated-routes.js
+++ src/module/authenticated-routes.js
@@ -1,4 +1,32 @@
-module.exports = (app) => function(req,res,next){
+const express = require("express");
+
+function checkAuthentication(req,res,next){
+  if(!req.isAuthenticated()){
+    res.status(403)
+      .json({message:"Login is required"})
+      .end();
+  }else{
+    next();
+  }
+}
+
+function configureApp(app,req,res,next) {
+  if(app.get("authenticatedRoutes")!==undefined) return;
   
+  console.log("Configuring authenticatedRoutes");
+
+  const authenticatedRouter = express.Router();
+  
+  authenticatedRouter.get("/user",(req,res)=>{
+    res.json(req.user).end();
+  });
+
+  app.use("/authenticated",checkAuthentication, authenticatedRouter);
+  
+  app.set("authenticatedRoutes",true);
+}
+
+module.exports = (app) => function(req,res,next){
+  configureApp(app,req,res,next);
   next();
 }
--- src/module/passport-strategy.js
+++ src/module/passport-strategy.js
@@ -1,7 +1,61 @@
 const config = require("../config.js");
+const passport = require('passport');
+const session = require("express-session");
+const LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
+
+function configureApp(app,req){
+ 
+  // Do nothing if passport is already configured
+  if(app.get("passport")!==undefined) {
+    return; 
+  }
+
+  console.log("Configuring passport linkedin strategy");
+  
+  // TODO should configure properly the session
+  app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: "SECRET"
+  }));
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  // TODO should configure properly serialization of user
+  passport.serializeUser(function (user, cb) {
+    cb(null, user);
+  });
+  passport.deserializeUser(function (obj, cb) {
+    cb(null, obj);
+  });
+
+  passport.use(new LinkedInStrategy({
+  clientID: config.oauth.linkedin.clientId,
+  clientSecret: config.oauth.linkedin.clientSecret,
+  callbackURL: "http://localhost:8080/auth/linkedin/callback",
+  scope: ['r_emailaddress', 'r_liteprofile'],
+  }, function(accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      // To keep the example simple, the user's LinkedIn profile is returned to
+      // represent the logged-in user. In a typical application, you would want
+      // to associate the LinkedIn account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
+    })
+  }));
+
+  app.get("/auth/linkedin", passport.authenticate("linkedin"));
+  app.get("/auth/linkedin/callback", passport.authenticate("linkedin",{
+    successRedirect: '/',
+    failureRedirect: '/login'
+  }));
+
+  app.set("passport",passport);
+}
 
 module.exports = (app) => function(req,res,next){
-  console.log(config.oauth.linkedin.clientId);
-  console.log(config.oauth.linkedin.clientSecret.substring(0,2));
+  configureApp(app,req);
   next();
 }
